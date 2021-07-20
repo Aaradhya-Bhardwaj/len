a6d1ec5b01af89d0d70530171f6ac9163c4c1679
Aaradhya-Bhardwaj <87181440+Aaradhya-Bhardwaj@users.noreply.github.com>
2021-07-20 11:22:50 +0530

    Update settings.py

--- Playlist/settings.py
+++ Playlist/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dkvb%(#emebq(&ztkax^ni1_0vb!uifg=+e&+wa@qc_^oa9no5'
+with open('local_settings.py', 'r) as file:
+          secret_key = file.read().strip()
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
